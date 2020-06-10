a80bf1e63032fdc9ca33f4b19c4240369985fc46
Innolize <Innolize@users.noreply.github.com>
2020-06-09 21:33:15 -0300

    test

---
+++ build/precache-manifest.50f6762d9da90cd8274991bbb167375a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248abe3b7224e76f4d8ff9b97e53545d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b217727c6a9c31a8ef9",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0b217727c6a9c31a8ef9",
+    "url": "/static/js/2.69792990.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.69792990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eeedbac96c26108efc9",
+    "url": "/static/js/main.6e82fd4c.chunk.js"
+  },
+  {
+    "revision": "4344a7482208d75157c2",
+    "url": "/static/js/runtime-main.0b4b9532.js"
+  }
+]);
