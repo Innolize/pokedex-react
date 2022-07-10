a84ea38c607e4a95a1082dad5b7a1975b91e283e
Roman Mettler <Roman.Mettler@gmail.com>
2022-07-10 12:23:40 -0300

    Agrega Prettier como dev-dependency

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
-import Pokedex from './components/Pokedex';
-import Pokemon from './components/Pokemon/Pokemon';
+import React from "react";
+import styled from "@emotion/styled";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import Pokedex from "./components/Pokedex";
+import Pokemon from "./components/Pokemon/Pokemon";
 
 const Header = styled.div`
-height:10vh;
-width: 100%;
-background: green;
-display:flex;
+  height: 10vh;
+  width: 100%;
+  background: green;
+  display: flex;
 `;
 
 const Link = styled(NavLink)`
-padding: 0.5em;
+  padding: 0.5em;
   background: none;
   border: none;
   border-radius: 1em;
   outline: none;
   color: white;
   text-decoration: none;
-  `;
+`;
 
 const Layout = styled.div`
-color: white;
-width: 100%;
-align-items: center;
-margin: auto;
-text-align:center;
-font-family: 'Roboto', sans-serif;
-`
+  color: white;
+  width: 100%;
+  align-items: center;
+  margin: auto;
+  text-align: center;
+  font-family: "Roboto", sans-serif;
+`;
 
 const Wrapper = styled.div`
-background-color: #000;
-width: 100%;
-min-height: 100vh;
-`
+  background-color: #000;
+  width: 100%;
+  min-height: 100vh;
+`;
 
 function App() {
   return (
@@ -47,14 +52,13 @@ function App() {
             </Link>
           </Header>
           <Layout>
-            <Route path="/" exact >
+            <Route path="/" exact>
               <Pokedex></Pokedex>
             </Route>
-            <Route path="/pokemon/:pokemonSeleccionado" >
+            <Route path="/pokemon/:pokemonSeleccionado">
               <Pokemon></Pokemon>
             </Route>
           </Layout>
-
         </Router>
       </Wrapper>
     </>
