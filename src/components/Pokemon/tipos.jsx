e0cb93f8ddc7862a3e7ada18f5cc80c280518805
Roman Mettler <Roman.Mettler@gmail.com>
2022-07-10 20:28:41 -0300

    Muestra iconos de tipos

--- src/components/Pokemon/tipos.jsx
+++ src/components/Pokemon/tipos.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import { useFetchReducer } from '../../customHooks/useFetch';
-import { fetchArrayTipos } from '../../utilidades';
-import { Spinner } from 'react-bootstrap';
+import React from "react";
+import { useFetchReducer } from "../../customHooks/useFetch";
+import { fetchArrayTipos } from "../../utilidades";
+import { Spinner } from "react-bootstrap";
+import styled from "@emotion/styled";
 
+const ImagenTipo = styled.img`
+  height: 50px;
+`;
+
+const ContenedorTipos = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 const TiposTraducidos = ({ tipos }) => {
-    const { data, loading, error } = useFetchReducer(fetchArrayTipos, tipos)
+  const { data, loading, error } = useFetchReducer(fetchArrayTipos, tipos);
 
-
-    if (loading)
-        return (<Spinner animation="border" role="status">
-            <span className="sr-only">Loading...</span>
-        </Spinner>)
-    if (error)
-        return (<div>error</div>)
-    if (data)
-        console.log(data)
+  if (loading)
     return (
-        <div className="d-flex">
-            {data.map((x, i) => <p key={i} style={{paddingRight:"5px"}}>{x}</p>)}
-        </div>
-    )
-}
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    );
+  if (error) return <div>error</div>;
+  if (data) console.log(data);
+  return (
+    <ContenedorTipos>
+      {data.map((x, i) => (
+        <ImagenTipo
+          key={i}
+          src={`/tipo-iconos/${x.toLowerCase()}.png`}
+        ></ImagenTipo>
+      ))}
+    </ContenedorTipos>
+  );
+};
 
-export default TiposTraducidos
+export default TiposTraducidos;
