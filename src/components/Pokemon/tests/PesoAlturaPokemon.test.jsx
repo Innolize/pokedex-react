3aa320f7c8d0173312ae5b5de83145e88fbbf06f
Roman Mettler <Roman.Mettler@gmail.com>
2022-07-12 16:06:52 -0300

    Mueve test de PesoAlturaPokemon a carpeta tests

--- src/components/Pokemon/tests/PesoAlturaPokemon.test.jsx
+++ src/components/Pokemon/tests/PesoAlturaPokemon.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
-import { PesoAltura } from "./PesoAlturaPokemon";
+import { PesoAltura } from "../PesoAlturaPokemon";
 
 test("renderiza contenido", () => {
   const props = { peso: 11, altura: 20 };
