1592714443a2ece464174fb9483355a9485f213a
Roman Mettler <Roman.Mettler@gmail.com>
2022-07-12 18:46:35 -0300

    Corrige test de PesoAlturaPokemon

--- src/components/Pokemon/tests/PesoAlturaPokemon.test.jsx
+++ src/components/Pokemon/tests/PesoAlturaPokemon.test.jsx
@@ -1,11 +1,20 @@
+import { PesoAltura } from "../PesoAlturaPokemon";
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
-import { PesoAltura } from "../PesoAlturaPokemon";
 
-test("renderiza contenido", () => {
-  const props = { peso: 11, altura: 20 };
-  render(<PesoAltura props={props}></PesoAltura>);
-  screen.getByText("Altura");
-  screen.getByText("Peso");
+describe("PesoAlturaPokemon", () => {
+  it("Renderiza peso y altura correctamente ", () => {
+    const props = { peso: 11, altura: 20 };
+    const alturaAMetros = 2;
+    render(<PesoAltura peso={props.peso} altura={props.altura}></PesoAltura>);
+    const elementoAltura = screen.getByText(alturaAMetros.toString(), {
+      exact: false,
+    });
+    const elementoPeso = screen.getByText(props.peso.toString(), {
+      exact: false,
+    });
+    expect(elementoAltura.textContent).toBe(`Altura: ${2} m`);
+    expect(elementoPeso.textContent).toBe(`Peso: ${11} Kg`);
+  });
 });
